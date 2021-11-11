9c9aaf8479ea48c488b1e9b856d37b4a7479f220
thiaguinhoo <thiagoluizsog@gmail.com>
2021-11-11 17:56:36 -0300

    add standard

--- server-authenticate/jest.config.js
+++ server-authenticate/jest.config.js
@@ -2,5 +2,5 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.resolve(__dirname),
-  testMatch: ['<rootDir>/tests/**/*.test.js'],
+  testMatch: ['<rootDir>/tests/**/*.test.js']
 }
